42f36b513e7e43e48ec4f0f8e52daed8209fce3a
Stevan Little <stevan.little@gmail.com>
2026-01-11 11:04:39 +0100

    cleanup

--- lib/Opal/Environment.pm
+++ lib/Opal/Environment.pm
@@ -6,6 +6,70 @@ use Opal::Term;
 use Opal::Term::Kontinue;
 use Opal::Machine;
 
+my @core = qw[
+    bool? true false
+        and or not
+
+    num?
+        + - * / mod == != < <= > >= <=>
+        sin cos abs trunc
+
+    str?
+        ~ eq ne gt ge lt le cmp
+        substr char-at
+
+    list? list
+    pair? pair
+    nil?  nil
+        cons first second rest
+
+    tuple? tuple
+        size at
+
+    array? array
+        array-get
+        length push pop shift unshift
+
+    hash? hash
+        hash-get
+        exists keys values
+
+    callable?
+    native?
+    fexpr?
+
+    lambda? lambda
+
+    quote let do if throw try
+];
+
+my @mutable = qw[
+    def defun
+
+    set! array-set! hash-set!
+];
+
+my @console = qw[
+    print say warn readline repl
+];
+
+my @file_io = qw[
+    open close
+    read readline slurp
+    write spew
+
+    opendir closedir
+    read
+
+    -e -f -d -s
+];
+
+my @actor_ipc = qw[
+    send recv
+    signal timeout
+    sleep
+];
+
 class Opal::Environment {
 
 
